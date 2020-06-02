d0b31e70eea4d572ad1a0655342cf7dfaf72edfb
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2020-06-03 00:56:21 +0300

    add history add serveStatic add fs set headers

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const history = require('connect-history-api-fallback')
+const serveStatic = require('serve-static')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const fs = require('fs')
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  next()
+})
 
 app.use(morgan('short'))
+app.use(history())
+app.use(serveStatic('dist'))
 
 process.sql = mysql.createConnection({
   host: 'den1.mysql3.gear.host',
@@ -12,12 +22,11 @@ process.sql = mysql.createConnection({
   database: 'stalasmysql'
 })
 
-const fs = require('fs')
-fs.readdirSync('./routes/').forEach(file => {
+fs.readdirSync('./app/routes/').forEach(file => {
   app.use(require(`./routes/${file}`))
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3003
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
