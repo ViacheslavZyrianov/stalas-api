e2b362b116572b6eb56cdfc035f304f8a517be9b
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2020-06-03 00:56:38 +0300

    add endpoint product-columns

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,18 @@
 const router = require('express').Router()
 
 router.get('/products', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
   process.sql.query('SELECT * FROM products', (err, rows) => {
+    if (err) {
+      console.log('Error occured', err)
+      res.sendStatus(500)
+    }
+    console.log('query parameters', req.query)
+    res.json(rows)
+  })
+})
+
+router.get('/product-columns', (req, res) => {
+  process.sql.query('SELECT * FROM product_columns', (err, rows) => {
     if (err) {
       console.log('Error occured', err)
       res.sendStatus(500)
